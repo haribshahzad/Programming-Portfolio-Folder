0c5738ed854606824109e35a5a275ae5f9f8d536
haribshahzad <harib.shahzad2003@gmail.com>
2026-01-12 11:53:25 +0400

    commit Add vending machine implementation in Python
    def main():
    print("Vending Machine module added.")
    if __name__=="__main__":
    main()

---
+++ Vending_Machine/vending_machine.py
@@ -0,0 +1,7 @@
+print ("Welcome to the Vending Machine")
+
+my_items= {
+'1':{'name':'Skittles', 'price':3.50, 'stock':10}}, 
+'2':{'name':'Lays', 'price':1.50, 'stock':10},
+'3':{'name':'Red_Bull', 'price':7.50, 'stock':10}
+}
